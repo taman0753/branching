5041874db097a26bc1e6929a995cb421863c0cd2
taman0753 <taman0753@gmail.com>
2019-03-27 12:43:10 +0530

    commit2 in new branch2

--- 1.cpp
+++ 1.cpp
@@ -2,11 +2,16 @@
 using namespace std;
 class test
 {
-	private :
+	public:
 		int marks1,marsk2;
 		void percentage();
 		void totalmarks();
  };
+void percentage()
+{
+	int per;
+	per = tm/500*100;
+}
 int main()
 { 
 	test o1,o2;
